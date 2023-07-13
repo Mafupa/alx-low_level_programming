e4a120008529cea3f36ea085ca4b3250ede5fd71
Mafupa <ryankarera@gmail.com>
2023-07-13 09:26:49 -0700

    0x00 Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of stuff
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)", sizeof(char));
+	printf("Size of a int: %i byte(s)", sizeof(int));
+	printf("Size of a long int: %i byte(s)", sizeof(long int));
+	printf("Size of a long long: %i byte(s)", sizeof(long long));
+	printf("Size of a float: %i byte(s)", sizeof(float));
+	return (0);
+}
