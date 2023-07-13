c693580980f1efd82b5cbd906814849aab821760
Mafupa <ryankarera@gmail.com>
2023-07-13 09:13:37 -0700

    0x00 Task 4 Understanding Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints stuff
+ *
+ * Description: Prints Programming is like bulding a multilingual puzzle
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
