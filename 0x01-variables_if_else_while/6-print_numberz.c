f2fcf8cc6873b84c7837f3178e8964744b2ddfab
Mafupa <ryankarera@gmail.com>
2023-07-14 08:48:32 -0700

    0x01 Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int cur = 0;
+
+	for (; cur < 10; cur++)
+	{
+		putchar(cur+'0');
+	}
+	putchar('\n');
+	return (0);
+}
