d4f54db7db4183dbcd21f745bc73aa7d0497284e
Mafupa <ryankarera@gmail.com>
2023-07-14 10:57:18 -0700

    0x01 Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints two by two combinaison of two digit numbers
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int n = 0, m = 1;
+
+	for (; n < 100; n++)
+	{
+		for (m = n + 1; m < 100; m++)
+		{
+			if (m != 0 || n != 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(n / 10 + '0');
+			putchar(n % 10 + '0');
+
+			putchar(' ');
+			putchar(m / 10 + '0');
+			putchar(m % 10 + '0');
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
