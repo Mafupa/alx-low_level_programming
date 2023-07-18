179cb7cb2af9f69bae4750a43a81a4bd68ce8d45
Mafupa <ryankarera@gmail.com>
2023-07-18 13:41:16 -0700

    0x02 Task 9 come on

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,15 +10,15 @@ void times_table(void)
 
 	for (t = 0; t < 10; t++)
 	{
-		for (n = 0; n < 10; n++)
+
+		_putchar('0');
+		for (n = 1; n < 10; n++)
 		{
-			if (n != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(',');
+			_putchar(' ');
+
 			x = t * n;
-			if (x < 10 && n != 0)
+			if (x < 10 )
 				_putchar(' ');
 			else
 				_putchar((x / 10) + '0');
