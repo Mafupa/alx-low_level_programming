dcc92a2919e03a0f8a2587897f035fadb28f3aab
Mafupa <ryankarera@gmail.com>
2023-07-20 16:17:09 -0700

    0x04 Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - find the largest prime factor
+ *
+ */
+int main(void)
+{
+	long int n = 612852475143, d = 2, bg = 0;
+
+	while (n > 1)
+	{
+		while (n % d == 0)
+		{
+			if (d > bg)
+				bg = d;
+			n = n / d;
+		}
+		d = d + 1;
+	}
+	printf("%ld", bg);
+	return (0);
+}
