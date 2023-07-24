d6f14187970160dda4894d860d290cf71dac3e8a
Mafupa <ryankarera@gmail.com>
2023-07-24 14:25:20 -0700

    0x05 Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strcpy - copies the string pointed to by src to dest
+ * @dest: destination
+ * @src: source copied from
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
