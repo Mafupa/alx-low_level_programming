1dbc79067125e6377fccedbab7a20a402b7f66d6
Mafupa <ryankarera@gmail.com>
2023-07-24 11:51:07 -0700

    0x05 Task 0 betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - takes a pointer to an int as
+ * parameter and updates the value it points to to 98
  * @n: the pointer
  */
 void reset_to_98(int *n)
