8330fc43eb5f2da097e6b3c243d1ba76de41efff
Mafupa <ryankarera@gmail.com>
2023-07-24 13:44:59 -0700

    0x05 Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+			printf("%c",str[i]);
+		i++;
+	}
+	printf("\n");
+}
