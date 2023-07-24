75e4565209384021f0cb6274d2145407b6ed7b5f
Mafupa <ryankarera@gmail.com>
2023-07-24 13:36:46 -0700

    0x05 Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char t;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	
+	for (; i < len / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = t;
+	}
+}
