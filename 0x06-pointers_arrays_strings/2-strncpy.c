e57979ca31c041e4f132a9613b03e42c91d43315
Mafupa <ryankarera@gmail.com>
2023-07-27 10:37:07 -0700

    0x06 Task 2 < ! <= : <

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,9 +9,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (; i <= n && src[i]; i++)
+	for (; i < n && src[i]; i++)
 		dest[i] = src[i];
-	for (; i <= n; i++)
+	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
 }
