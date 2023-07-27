03ba65969f8e6d34624b7f60d17f23ae24c3f0dc
Mafupa <ryankarera@gmail.com>
2023-07-27 11:52:07 -0700

    0x06 Task 5 first attempt

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ * string_toupper -  changes all lowercase letters of a string to uppercase
+ * @a: the string
+ * Return: Pointer to the string
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}
