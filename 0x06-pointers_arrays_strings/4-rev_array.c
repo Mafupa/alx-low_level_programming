e98feb2924aae83805fe4e0b8fe73617ed46653a
Mafupa <ryankarera@gmail.com>
2023-07-27 11:43:14 -0700

    0x06 Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: the array
+ * @n: the array's size
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, j = n -1, tmp;
+
+	for (; i < n / 2; i++,j--)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+}
