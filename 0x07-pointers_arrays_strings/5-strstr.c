4b8c9a2b77cab6a819d550432b4245b19b8e323c
Mafupa <ryankarera@gmail.com>
2023-07-31 15:54:23 -0700

    0x07 Task 5 betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j]; j++)
 		{
-			if (haystack[i+j] != needle[j])
+			if (haystack[i + j] != needle[j])
 				break;
 		}
 		if (needle[j] == '\0')
