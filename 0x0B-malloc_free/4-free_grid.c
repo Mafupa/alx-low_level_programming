d2bb7061f81a25273c81b9ca37ec8816e921b910
Mafupa <ryankarera@gmail.com>
2023-08-07 06:20:13 -0700

    0x0B Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_grid - frees a 2d grid
+ * @grid: the grid
+ * @height: the height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid == NULL || height == 0)
+		return;
+	for (; i < height; i++)
+		free(grid[i])
+	free(grid);
+}
