7e0bd7a42f1ed6f8a41e4e25ffe56aa04bea7712
Mafupa <ryankarera@gmail.com>
2023-08-11 12:27:07 -0400

    0x0C Task 4 attempt

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,10 +27,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 	old_ptr = ptr;
+	if (new_size < old_size)
+		old_size = new_size;
 	for (i = 0; i < old_size; i++)
 		new_ptr[i] = old_ptr[i];
 	free(ptr);
 	return (new_ptr);
 }
-		
-
