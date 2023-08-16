ad79be0ac7a0db520911978508195e579d3e5178
Mafupa <ryankarera@gmail.com>
2023-08-16 12:06:33 -0700

    0x0F Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: the funtion
+ * Return: Index of the first element that matches, -1 if no element matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return i;
+	}
+	return -1;
+}
