248130371e4c35023fcb17021915fe26b00398b4
Mafupa <ryankarera@gmail.com>
2023-08-18 08:05:52 -0700

    0x0F Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (98);
 	}
-	if ((argv[2] == "/" || argv[2] == "%") && atoi(argv[3]))
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		return (100);
