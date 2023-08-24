019eb32af1aff8ca538f348dd95ad234acd6d618
Mafupa <ryankarera@gmail.com>
2023-08-24 15:18:37 -0400

    0x12 Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void myConstructor() __attribute__((constructor));
+
+void myConstructor()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
