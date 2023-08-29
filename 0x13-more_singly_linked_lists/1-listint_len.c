7dd3e83a792e2b2a483ed154b0f61f3601daad08
Mafupa <ryankarera@gmail.com>
2023-08-29 13:14:06 -0700

    0x13 Task 1 description

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * listint_len - returns the lenght of a listint_t linked list
+ * @h: the head of the listint_t
+ * Return: the length of the listint_t
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
