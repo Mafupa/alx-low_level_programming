8d62728e28ce02939eab9d65b71e9706ddb1cf59
Mafupa <ryankarera@gmail.com>
2023-11-04 01:24:07 +0200

    0x1A Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 		while (temp)
 		{
 			if (flag == 1)
-				printf(",");
+				printf(", ");
 			printf("'%s': '%s'", temp->key, temp->value);
 			flag = 1;
 			temp = temp->next;
