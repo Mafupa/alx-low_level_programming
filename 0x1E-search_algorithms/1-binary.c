309b7bce589ce5a90f61a2b4143ffc4e75ea8cd2
Mafupa <ryankarera@gmail.com>
2024-05-11 22:19:10 +0200

    0x1E Task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search a value in an array
+ *
+ * @array: array
+ * @size: size of array
+ * @value: value to search
+ * Return: index of value, -1 if not found
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, l, r;
+
+	if (!array)
+		return (-1);
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		printf("Search in array: ");
+		for (i = l; i < r; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = l + (r - l) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+	return (-1);
+}
+
