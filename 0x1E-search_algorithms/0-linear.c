279c1278772ce9ca771cf3144eba842cc1c6bfa5
Mafupa <ryankarera@gmail.com>
2024-05-11 21:34:42 +0200

    0x1E Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,13 +8,15 @@
  * @value: value searched
  * Return: index of value, -1 if array is NULL or value not in array
  */
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
 	size_t i;
 
 	if(!array)
 		return (-1);
-	for(i = 0; i < size; i++){
-		printf("Value checked array[%ld], = [%d]\n", i, array[i]);
+	for(i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if(array[i] == value)
 			return (i);
 	}
